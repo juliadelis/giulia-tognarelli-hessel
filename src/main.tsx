c8774f73a5276d1f129d1f1a4814afa9fd35df79
Julia Delis <juliadelis2@hotmail.com>
2025-11-04 17:07:45 -0300

    feat: google tag

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./app/App.tsx";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("AW-17658242454");
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
